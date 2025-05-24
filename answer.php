b2f80301f65ca7e8d284cbcff14026839f9ed28f
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-24 14:25:40 -0400

    EDITED

--- answer.php
+++ answer.php
@@ -8,8 +8,7 @@
   <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
-  <link rel="stylesh
-  eet" href="https://fonts.googleapis.com/icon?family=Material.Icons" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material.Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-deep_orange.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
@@ -40,21 +39,17 @@
 
         // ring the information from jsonData
         $iconCode = $jsonData['weather'][0]['icon'];
-        $location = $jsonData['name'];
-        $coordinate = $jsonData['coord'];
         $weatherMain = $jsonData['weather'][0]['main'];
         $weatherDescription = $jsonData['weather'][0]['description'];
-        $tempCelsius = ($jsonData['main']['temp'] - 273.15);
-        $windSpeed = $jsonData['wind']['speed'];
+        $tempKelvin = $jsonData['main']['temp'];
+        $tempCelsius = $tempKelvin - 273.15
 
         // output
         echo '<img src="https://openweathermap.org/img/wn/' . $iconCode . '@2x.png" alt="weather icon">';
-        echo '<p>Location: ' . $location . ' (lat: ' . $coordinate['lat'] . ', lon: ' . $coordinate['lon'] . ')</p>';
-        echo '<p>Weather: ' . $weatherMain . ' - ' . $weatherDescription . '</p>';
-        echo '<p>Temperature: ' . number_format($tempCelsius, 1) . ' °C</p>';
-        echo '<p>Wind Speed: ' . $windSpeed . ' m/s</p>';
+        echo 'Weather: ' . $weatherMain . '; ' . $weatherDescription . '<br />';
+        echo 'Temperature: ' . number_format($tempCelsius, 1) . ' °C';
       } else {
-        echo '<p>Error fetching weather data.</p>';
+        echo 'Error fetching weather data.';
       }
       ?>
   </div>
